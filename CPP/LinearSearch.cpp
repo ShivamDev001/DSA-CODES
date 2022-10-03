e2aeacddc6ab7fe2abc3753d788ae9b8a77a1572
Gautam Goyal <78250503+GautamGoyal2341@users.noreply.github.com>
2022-10-03 20:14:44 +0530

    Update LinearSearch.cpp

--- CPP/LinearSearch.cpp
+++ CPP/LinearSearch.cpp
@@ -14,7 +14,13 @@ bool search(int arr[], int size, int element){
 }
 
 int main(){
-    int arr[10] = {1,2,3,4,5,6,7,7,9,0};
+//     int arr[10] = {1,2,3,4,5,6,7,7,9,0};
+int arr[10];
+    for(int i = 0 ; i < 10 ; i++)
+    {
+        cin>>arr[i];
+    }
+    
 
     cout << " Enter key " << endl;
     int key;
